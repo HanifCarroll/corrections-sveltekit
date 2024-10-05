a8d268cfdc7ad64054a0973eddcd8a06dcbe48dc
Hanif Carroll <hanifcarroll@gmail.com>
2024-10-05 14:01:49 -0300

    Fix buttons on show page. Create corrections show and corrections new pages

--- src/routes/posts/[id]/+page.svelte
+++ src/routes/posts/[id]/+page.svelte
@@ -72,14 +72,23 @@ async function deletePost() {
                     </div>
                     <div class="mt-6 flex">
                         {#if post.user.id !== loggedInUser?.id}
-                            <Button variant="primary" on:click={() => goto(`/posts/${post.id}/corrections/new`)}>
+                            <button
+                                on:click={() => {
+                                    console.log("post.id", post.id);
+                                    goto(`/posts/${post.id}/corrections/new`);
+                                }}
+                                class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 text-white dark:text-gray-800 hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 border-transparent rounded-md font-semibold text-xs uppercase tracking-widest shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+                            >
                                 New Correction
-                            </Button>
+                            </button>
                         {/if}
                         {#if post.user.id === loggedInUser?.id}
-                            <Button variant="secondary" on:click={deletePost} class="text-red-600 border-red-600 hover:bg-red-50">
+                            <button
+                                on:click={deletePost}
+                                class="inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-300 border border-gray-300 dark:border-gray-500 hover:bg-gray-50 dark:hover:bg-gray-700 border-transparent rounded-md font-semibold text-xs uppercase tracking-widest shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 text-red-600 border-red-600 hover:bg-red-50"
+                            >
                                 Delete Post
-                            </Button>
+                            </button>
                         {/if}
                     </div>
                 </div>
